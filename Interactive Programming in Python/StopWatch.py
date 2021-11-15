8ec6ffe1eb5efc5f435b20c3397eb82af8ddd3f3
mhpanchal <60170780+mhpanchal@users.noreply.github.com>
2021-11-15 11:32:58 +0530

    Create StopWatch.py

---
+++ Interactive Programming in Python/StopWatch.py
@@ -0,0 +1,71 @@
+import simplegui as gui
+
+# define global variables
+interval = 100
+count = 0
+total_stops = 0
+succes_stops = 0
+stop = True
+
+# define helper function format that converts time
+# in tenths of seconds into formatted string A:BC.D
+def format(t):
+    tenth_sec = (t) % 10
+    sec = int(t / 10) % 10
+    minutes = int(t / 600) % 600
+    ten_min = int(t / 100) % 6
+    string = str(minutes) + ":" + str(ten_min) + str(sec) + "." + str(tenth_sec)
+    return string
+    
+# define event handlers for buttons; "Start", "Stop", "Reset"
+def Start():
+    global count, stop
+    stop = False
+    timer.start()
+    
+def Stop():
+    global total_stops, succes_stops, stop
+    if stop == False :
+        if count % 10 == 0 and count != 0 :
+            succes_stops += 1
+            total_stops += 1
+        elif count != 0 :
+            total_stops += 1
+    stopped = True
+    timer.stop()
+    
+def Reset():
+    global count, succes_stops, total_stops
+    count = 0
+    stop = True
+    total_stops = 0
+    succes_stops = 0
+    timer.stop()
+
+
+# define event handler for timer with 0.1 sec interval
+def tick():
+    global count
+    count += 1
+
+# define draw handler
+def draw(canvas):
+    text = format(count)
+    canvas.draw_text( text, (45, 80), 25, "white")
+    canvas.draw_text(str(succes_stops) + '/' + str(total_stops), (25,30), 15, "lime")
+    
+# create frame
+frame = gui.create_frame("The Stopwatch Game", 150, 150)
+#frame.set_canvas_background()
+
+
+# register event handlers
+frame.add_button("Start", Start, 100)
+frame.add_button("Stop", Stop, 100)
+frame.add_button("Reset", Reset, 100)
+frame.set_draw_handler(draw)
+timer = gui.create_timer(interval, tick)
+
+
+# start frame
+frame.start()
